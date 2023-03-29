4606a269a1ec4f8dd9a41fea3c74cb44ec506fb5
Mmhh24122045 <dsi.elleuch.hasna@gmail.com>
2023-03-29 11:24:41 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state={
+    Person :{ 
+      fullName:'hasna',
+      bio: 'sssss',
+      profession: 'developpment',
+      
+    } ,
+    show : true,
+      count:0,
+  }
+ 
+  // func = () =>{ this.setState({this.Person.fullName})};
+  handleShow=()=>this.setState({show:!this.state.show});
+
+componentDidMount(){
+  console.log('the compenent is rendered')
 }
 
-export default App;
+componentDidUpdate(){
+  console.log('the sate is updated')
+}
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleShow}>{this.state.show ? 'Button' : 'show'}
+        </button>
+        <h3>{this.state.count}</h3>
+        {
+          this.state.show && 
+  
+        <div>
+
+        <h2>FullName:{this.state.Person.fullName}</h2>
+        <h2>Bio:{this.state.Person.bio}</h2>
+        <img src='https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg'/> 
+        <h2>Profession:{this.state.Person.profession}</h2>
+
+        </div>
+  }
+         componentDidMount() {
+
+         setInterval(() => this.setState({count:this.state.count+1}), 1000)
+    }  
+
+      </div>
+    )
+  }
+}
+
